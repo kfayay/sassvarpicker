5960e07be53fc05014c5d9efaf0d3214e4bda5f3
kfayay <kfayay@gmail.com>
2023-03-20 23:52:33 +0800

    feat: add color test function

--- out/extension.js
+++ out/extension.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.PropertyInfo = exports.ColorPropertyPicker = exports.activate = void 0;
+exports.ColorPropertyPicker = exports.activate = void 0;
 const vscode = require("vscode");
 function activate(context) {
     // const varFilePath = vscode.workspace
     //   .getConfiguration('sassvarpicker')
     //   .get('varFilePath');
-    context.subscriptions.push(vscode.languages.registerCodeActionsProvider('scss', new ColorPropertyPicker(), {
+    context.subscriptions.push(vscode.languages.registerCodeActionsProvider("scss", new ColorPropertyPicker(), {
         providedCodeActionKinds: ColorPropertyPicker.providedCodeACtionKinds,
     }));
 }
@@ -17,19 +17,29 @@ class ColorPropertyPicker {
         this.pathList = pathList;
     }
     provideCodeActions(document, range, context, token) {
-        if (!this.isColorProperty(document, range)) {
+        if (!this.isCursorInColorProperty(document, range)) {
             return;
         }
-        const replaceColor = this.createFix(document, range, '$test');
+        const replaceColor = this.createFix(document, range, "$test");
         return [replaceColor];
     }
-    isColorProperty(document, range) {
+    // 定义一个函数来判断当前行是否存在 css 颜色属性，并且光标是否在颜色属性区间内
+    isCursorInColorProperty(document, range) {
         const start = range.start;
-        const line = document.lineAt(start.line);
-        return line.text[start.character] === '#';
+        const lineText = document.lineAt(start.line).text;
+        const colorRegex = /(#(?:[a-f\d]{3}){1,2}|rgb\(\d{1,3},\s*\d{1,3},\s*\d{1,3}\)|efQ6qDy8J6VDiYXrgba\(\d{1,3},\s*\d{1,3},\s*\d{1,3},\s*(?:0?\.\d+|1)\))/i;
+        const match = colorRegex.exec(lineText);
+        if (!match) {
+            return false;
+        }
+        // 获取匹配结果在当前行中的起始和结束位置
+        const colorStart = match.index;
+        const colorEnd = colorStart + match[0].length;
+        // 判断光标是否在匹配结果的区间内，如果是返回 true，否则返回 false
+        return start.character >= colorStart && start.character <= colorEnd;
     }
     createFix(document, range, color) {
-        const fix = new vscode.CodeAction('Convert color to variable', vscode.CodeActionKind.QuickFix);
+        const fix = new vscode.CodeAction("Convert color to variable", vscode.CodeActionKind.QuickFix);
         fix.edit = new vscode.WorkspaceEdit();
         fix.edit.replace(document.uri, new vscode.Range(range.start, range.start.translate(0, 7)), color);
         return fix;
@@ -39,24 +49,4 @@ exports.ColorPropertyPicker = ColorPropertyPicker;
 ColorPropertyPicker.providedCodeACtionKinds = [
     vscode.CodeActionKind.QuickFix,
 ];
-class PropertyInfo {
-    provideCodeActions(document, range, context, token) {
-        return context.diagnostics
-            .filter((diagnostic) => diagnostic.code === '#000')
-            .map((diagnostic) => this.createCommandCodeAction(diagnostic));
-    }
-    createCommandCodeAction(diagnostic) {
-        const action = new vscode.CodeAction('Learn more...', vscode.CodeActionKind.QuickFix);
-        action.command = {
-            command: 'sassvarpicker.command',
-            title: 'Learn more about emojis',
-            tooltip: 'test',
-        };
-        action.diagnostics = [diagnostic];
-        action.isPreferred = true;
-        return action;
-    }
-}
-exports.PropertyInfo = PropertyInfo;
-PropertyInfo.providedCodeActionKinds = [];
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-import type { VariableInfo } from './model';
+import type { VariableInfo } from "./model";
 
 export function activate(context: vscode.ExtensionContext) {
   // const varFilePath = vscode.workspace
@@ -9,7 +9,7 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(
     vscode.languages.registerCodeActionsProvider(
-      'scss',
+      "scss",
       new ColorPropertyPicker(),
       {
         providedCodeActionKinds: ColorPropertyPicker.providedCodeACtionKinds,
@@ -34,20 +34,35 @@ export class ColorPropertyPicker implements vscode.CodeActionProvider {
     context: vscode.CodeActionContext,
     token: vscode.CancellationToken
   ): vscode.ProviderResult<(vscode.CodeAction | vscode.Command)[]> {
-    if (!this.isColorProperty(document, range)) {
+    if (!this.isCursorInColorProperty(document, range)) {
       return;
     }
 
-    const replaceColor = this.createFix(document, range, '$test');
+    const replaceColor = this.createFix(document, range, "$test");
 
     return [replaceColor];
   }
 
-  private isColorProperty(document: vscode.TextDocument, range: vscode.Range) {
+  // 定义一个函数来判断当前行是否存在 css 颜色属性，并且光标是否在颜色属性区间内
+  private isCursorInColorProperty(
+    document: vscode.TextDocument,
+    range: vscode.Range
+  ) {
     const start = range.start;
-    const line = document.lineAt(start.line);
+    const lineText = document.lineAt(start.line).text;
+    const colorRegex =
+      /(#(?:[a-f\d]{3}){1,2}|rgb\(\d{1,3},\s*\d{1,3},\s*\d{1,3}\)|efQ6qDy8J6VDiYXrgba\(\d{1,3},\s*\d{1,3},\s*\d{1,3},\s*(?:0?\.\d+|1)\))/i;
+    const match = colorRegex.exec(lineText);
 
-    return line.text[start.character] === '#';
+    if (!match) {
+      return false;
+    }
+
+    // 获取匹配结果在当前行中的起始和结束位置
+    const colorStart = match.index;
+    const colorEnd = colorStart + match[0].length;
+    // 判断光标是否在匹配结果的区间内，如果是返回 true，否则返回 false
+    return start.character >= colorStart && start.character <= colorEnd;
   }
 
   private createFix(
@@ -56,7 +71,7 @@ export class ColorPropertyPicker implements vscode.CodeActionProvider {
     color: string
   ): vscode.CodeAction {
     const fix = new vscode.CodeAction(
-      'Convert color to variable',
+      "Convert color to variable",
       vscode.CodeActionKind.QuickFix
     );
     fix.edit = new vscode.WorkspaceEdit();
@@ -67,39 +82,5 @@ export class ColorPropertyPicker implements vscode.CodeActionProvider {
     );
 
     return fix;
-  }
-}
-
-export class PropertyInfo implements vscode.CodeActionProvider {
-  public static readonly providedCodeActionKinds = [];
-
-  provideCodeActions(
-    document: vscode.TextDocument,
-    range: vscode.Range | vscode.Selection,
-    context: vscode.CodeActionContext,
-    token: vscode.CancellationToken
-  ): vscode.CodeAction[] {
-    return context.diagnostics
-      .filter((diagnostic) => diagnostic.code === '#000')
-      .map((diagnostic) => this.createCommandCodeAction(diagnostic));
-  }
-
-  private createCommandCodeAction(
-    diagnostic: vscode.Diagnostic
-  ): vscode.CodeAction {
-    const action = new vscode.CodeAction(
-      'Learn more...',
-      vscode.CodeActionKind.QuickFix
-    );
-
-    action.command = {
-      command: 'sassvarpicker.command',
-      title: 'Learn more about emojis',
-      tooltip: 'test',
-    };
-    action.diagnostics = [diagnostic];
-    action.isPreferred = true;
-
-    return action;
   }
 }
